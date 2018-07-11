a16383d876349539246fe9b28b667bde9d414952
Juanjo Salvador <juanjosalvador@netc.eu>
2018-07-11 16:20:36 +0200

    added PIP support

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 Simple GTK3 application that brings you cats to your desktop.
 
-
 ![](screenshot.png)
+
+## Development
+
+    $ make install
+    $ make run
--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ setup(
     author='Juanjo Salvador',
     author_email='juanjosalvador@netc.eu',
     packages=find_packages(),
+    package_data={'gnomeCats':['application.ui']},
     classifiers=[
         'Development Status :: 1 - Planning',
     ],
+    scripts=['bin/gnome-cats'],
     install_requires=[
         'PyGObject==3.28.1',
     ],
