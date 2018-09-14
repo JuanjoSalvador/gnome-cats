bea7f71402261e92b2e4550d12aff9019f67b3f0
Alejandro Domínguez <adomu@net-c.com>
2018-09-14 15:34:54 +0200

    Code cleanup

--- src/__main__.py
+++ src/__main__.py
@@ -1,61 +1,34 @@
 #!/usr/bin/env python3
 
-import gi, urllib.request, threading, os
+import gi, urllib.request, threading
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GObject, GdkPixbuf
 
-class Api():
-    def get_cat():
-        urllib.request.urlretrieve('http://cataas.com/cat', 'cat.jpg')
+def get_cat():
+    urllib.request.urlretrieve('http://cataas.com/cat', 'cat.jpg')
 
-class ApplicationWindow(Gtk.Window):
-    """
-    ApplicationWindow is the main window instance
-    """
+class Handlers():
+    def change_picture(self, picture):
+        threading.Thread(target=get_cat(), daemon=True).start()
 
-    builder = Gtk.Builder()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale('cat.jpg',
+                                                         width=450,
+                                                         height=600,
+                                                         preserve_aspect_ratio=True)
+        picture.set_from_pixbuf(pixbuf)
+        picture.show()
 
-    def __init__(self):
-        app_builder = ApplicationWindow.builder
+    def quit(*args):
+        Gtk.main_quit() 
 
-        app_builder.add_from_file(os.path.join(os.path.dirname(__file__), "application.ui"))
 
-        app_builder.connect_signals(ApplicationWindow)
+app = Gtk.Builder()
+app.add_from_file("application.ui")
+app.connect_signals(Handlers())
 
-        window = app_builder.get_object("mainWindow")
-        window.show_all()
+window = app.get_object("mainWindow")
+window.show_all()
 
-        thread = threading.Thread(target=Api.get_cat())
-        thread.daemon = True
-        thread.start()
+app.get_object("new_cat").clicked()
 
-        image = ApplicationWindow.builder.get_object('cat_picture')
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale('cat.jpg', width=450, height=600, preserve_aspect_ratio=True)
-        image.set_from_pixbuf(pixbuf)
-        image.show()
-
-    def on_button_meow(self, *args):
-        image = ApplicationWindow.builder.get_object('cat_picture')
-
-        thread_image = threading.Thread(target=Api.get_cat())
-        thread_image.daemon = True
-        thread_image.start()
-
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale('cat.jpg', width=450, height=600, preserve_aspect_ratio=True)
-        image.set_from_pixbuf(pixbuf)
-        image.show()
-
-    def on_button_close(self, *args):
-        """
-        Close the server and client.
-        """
-
-        Gtk.main_quit(*args)
-
-"""
-Main window for GNOME Cats
-"""
-app = ApplicationWindow()
-
-# Main loop
 Gtk.main()
