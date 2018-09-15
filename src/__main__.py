cfd781ab342705ef78241ee18caeaaed3f0601d8
Alejandro Domínguez <adomu@net-c.com>
2018-09-15 10:29:41 +0200

    Add loading spinner and async image loading

--- src/__main__.py
+++ src/__main__.py
@@ -4,29 +4,42 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GdkPixbuf, Gio
 
-def get_cat_stream():
+def get_cat_file():
     return Gio.File.new_for_uri('http://cataas.com/cat')
 
-class Handlers():
-    def change_picture(self, picture):
-        cat = GdkPixbuf.Pixbuf.new_from_stream_at_scale(get_cat_stream().read(),
-                                                        width=450,
-                                                        height=600,
-                                                        preserve_aspect_ratio=True)
-        picture.set_from_pixbuf(cat)
-        picture.show()
+class App():
+    def __init__(self):
+        ui = Gtk.Builder()
+        ui.add_from_file("application.ui")
+        ui.connect_signals(self)
+
+        window = ui.get_object("mainWindow")
+        window.show_all()
+
+        self.load_spinner = ui.get_object("load_spinner")
+        self.load_new_image(ui.get_object("cat_image"))
+                              
+
+    def load_new_image(self, image):
+        image.hide()
+        self.load_spinner.start()
+        get_cat_file().read_async(0, None, self.on_ready_new_image, image)
+
+    def on_ready_new_image(self, cat_stream, result, image):
+        self.cat_stream = cat_stream.read_finish(result)
+        self.scale_and_show_image(image, image.get_allocation())
+        self.load_spinner.stop()
+
+    def scale_and_show_image(self, image, dest, *args):
+        image.set_from_pixbuf(GdkPixbuf.Pixbuf.new_from_stream_at_scale(
+                                    self.cat_stream,
+                                    width=dest.width,
+                                    height=dest.height,
+                                    preserve_aspect_ratio=True))
+        image.show()
 
     def quit(*args):
         Gtk.main_quit() 
 
-
-app = Gtk.Builder()
-app.add_from_file("application.ui")
-app.connect_signals(Handlers())
-
-window = app.get_object("mainWindow")
-window.show_all()
-
-app.get_object("new_cat").clicked()
-
+App()
 Gtk.main()
