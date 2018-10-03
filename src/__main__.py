193b27b0d2fab4acd7945f114d76c835ab0713ea
Alejandro Domínguez <adomu@net-c.com>
2018-10-03 02:01:33 +0200

    Add ability to save images

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,7 @@ import gi, sys
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GdkPixbuf, Gio
 
-def get_cat_file():
+def get_cat_ref():
     return Gio.File.new_for_uri('https://cataas.com/cat')
 
 class App(Gtk.Application):
@@ -12,6 +12,7 @@ class App(Gtk.Application):
         super().__init__(*args,
                          application_id="com.jotadevs.GnomeCats",
                          **kwargs)
+        self.cat_file = Gio.File.new_tmp()[0]
         self.cat_pixbuf = None
 
     def do_startup(self):
@@ -33,28 +34,63 @@ class App(Gtk.Application):
         pass
 
     def load_and_show_new_image(self, image):
+        self.ui.get_object("save_image").hide()
         image.hide()
         self.load_spinner.start()
-        get_cat_file().read_async(0, None, self.on_ready_new_image, image)
+        get_cat_ref().read_async(0, None, self.on_ready_new_image, image)
 
     def on_ready_new_image(self, cat, res, image):
-        self.cat_pixbuf = GdkPixbuf.Pixbuf.new_from_stream(cat.read_finish(res))
+        self.cat_file.replace(
+                          None,
+                          False,
+                          Gio.FileCreateFlags.NONE,
+                          None
+                    ).splice(
+                          cat.read_finish(res),
+                          Gio.OutputStreamSpliceFlags.CLOSE_TARGET,
+                          None)
+        self.cat_pixbuf = GdkPixbuf.Pixbuf.new_from_stream(self.cat_file.read())
         self.load_spinner.stop()
         self.scale_image(image, image.get_allocation())
         image.show()
+        self.ui.get_object("save_image").show()
 
     def scale_image(self, image, dest, *args):
         if self.cat_pixbuf:
-            factor = min(dest.width / self.cat_pixbuf.get_width(),
-                         dest.height / self.cat_pixbuf.get_height())
+            fact = min(dest.width / self.cat_pixbuf.get_width(),
+                       dest.height / self.cat_pixbuf.get_height())
 
-            scaled = GdkPixbuf.Pixbuf.scale_simple(
-                                          self.cat_pixbuf,
-                                          self.cat_pixbuf.get_width() * factor,
-                                          self.cat_pixbuf.get_height() * factor,
-                                          GdkPixbuf.InterpType.BILINEAR
-                                      )
-            image.set_from_pixbuf(scaled)
+            image.set_from_pixbuf(GdkPixbuf
+                                      .Pixbuf
+                                      .scale_simple(
+                                           self.cat_pixbuf,
+                                           self.cat_pixbuf.get_width() * fact,
+                                           self.cat_pixbuf.get_height() * fact,
+                                           GdkPixbuf.InterpType.BILINEAR))
+
+    def save_image(self, parent):
+        if self.cat_file:
+            save_dialog = Gtk.FileChooserNative.new(None,
+                                                    parent,
+                                                    Gtk.FileChooserAction.SAVE,
+                                                    None,
+                                                    None)
+            jpg_filter = Gtk.FileFilter()
+            jpg_filter.add_pattern("*.jpg")
+            jpg_filter.add_mime_type("image/jpeg")
+            jpg_filter.set_name("JPEG image")
+            save_dialog.add_filter(jpg_filter)
+            if save_dialog.run() == Gtk.ResponseType.ACCEPT:
+                save_dialog.get_file(
+                          ).replace(
+                                None,
+                                False,
+                                Gio.FileCreateFlags.NONE,
+                                None
+                          ).splice(
+                                self.cat_file.read(),
+                                Gio.OutputStreamSpliceFlags.CLOSE_TARGET,
+                                None)
 
     def show_about(self, *args):
         self.ui.add_objects_from_file(Gio.File.new_for_path(__file__)
@@ -63,5 +99,9 @@ class App(Gtk.Application):
                                               + "/application.ui",
                                       ["aboutWindow"])
         self.ui.get_object("aboutWindow").present()
+
+    def do_shutdown(self):
+        self.cat_file.delete()
+        Gtk.Application.do_shutdown(self)
 
 App().run(sys.argv)
